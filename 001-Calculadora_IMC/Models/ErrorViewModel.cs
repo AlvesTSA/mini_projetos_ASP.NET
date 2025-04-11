a42c97b9b5db58495f2cfd681492f49501d8a9a9
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2025-04-10 22:24:28 -0300

    create calculator-imc

---
+++ 001-Calculadora_IMC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _001_Calculadora_IMC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
