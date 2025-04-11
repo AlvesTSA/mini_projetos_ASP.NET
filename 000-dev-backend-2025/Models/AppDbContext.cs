798277b113524aaeef3b6a5a30a6e9d8fb76685f
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2025-04-11 01:28:19 -0300

    UpDate-dev-backend

---
+++ 000-dev-backend-2025/Models/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _000_dev_backend_2025.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {   }
+        DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
---
+++ 000-dev-backend-2025/Models/Veiculo.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _000_dev_backend_2025.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo é obrigatório.")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "O campo é obrigatório.")]
+        public  int AnoModelo { get; set; }
+
+        [Required(ErrorMessage = "O campo é obrigatório.")]
+        public int AnoFabricacao { get; set; }
+
+    }
+}
